b25d4babb4fc0a9f07c98e5998828d79c48d649f
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2025-03-30 14:38:34 +0200

    updated structure for tailwind support

---
+++ temp/src/App.tsx
@@ -0,0 +1,16 @@
+import Background from './components/Background/Background';
+import MainFrame from './components/MainFrame/MainFrame';
+
+
+function App() {
+
+  return (
+    <>
+    <Background/>
+    <MainFrame/>
+    <title>Nightmare :3</title>
+    </>
+  )
+}
+
+export default App
