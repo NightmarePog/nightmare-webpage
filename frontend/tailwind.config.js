0823ae3b9780924655dfff326ae9c041d6e68bc4
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2025-03-30 13:41:58 +0200

    some testing and settings for Tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+import { Config } from 'tailwindcss';
+
+const config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: 'black',
+        secondary: '#fff',
+        primary2: '#d1d1d1',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
